19a7bbb01d3f97eb7b05be2ce86614f73af54ef9
Axel GMX <agmx183@gmail.com>
2024-06-25 13:38:26 +0200

    User Story #4 : Afficher le Prénom sur le dashboard

--- Sport-See-FR_Front-End/src/pages/Home/Home.js
+++ Sport-See-FR_Front-End/src/pages/Home/Home.js
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import emoji from "../../assets/emoji/emoji.png";
 import mockActivity from "../../utils/mockActivity";
 import mockAverageSessions from "../../utils/mockAverageSessions";
 import mockPerformance from "../../utils/mockPerformance";
@@ -9,26 +10,33 @@ import Sidebar from "./../../components/Sidebar/Sidebar";
 import "./Home.scss";
 
 function Home() {
+  const [userInfos, setUserInfos] = useState(null);
+
+  const userId = 18;
+
   const fetchUserInformations = () => {
-    const userInfos = mockUserInfos.find((user) => user.id === 18);
+    const userInfos = mockUserInfos.find((user) => user.id === userId);
     console.log("User Informations:", userInfos);
+    setUserInfos(userInfos);
   };
 
   const fetchUserActivityInformations = () => {
-    const activity = mockActivity.find((activity) => activity.userId === 18);
+    const activity = mockActivity.find(
+      (activity) => activity.userId === userId
+    );
     console.log("User Activity Informations:", activity);
   };
 
   const fetchUserAverageSessions = () => {
     const averageSessions = mockAverageSessions.find(
-      (session) => session.userId === 18
+      (session) => session.userId === userId
     );
     console.log("User Average Sessions:", averageSessions);
   };
 
   const fetchUserPerformance = () => {
     const performance = mockPerformance.find(
-      (performance) => performance.userId === 18
+      (performance) => performance.userId === userId
     );
     console.log("User Performance:", performance);
   };
@@ -45,6 +53,28 @@ function Home() {
       <Navbar />
       <div className="container">
         <Sidebar />
+        <div className="daily-container">
+          {userInfos ? (
+            <>
+              <h1 className="title-container-home">
+                Bonjour
+                <span className="name-container-home">
+                  {userInfos.userInfos.firstName}
+                </span>
+              </h1>
+              <p className="presentation-container-home">
+                Félicitation! Vous avez explosé vos objectifs hier
+                <img
+                  className="emoji"
+                  src={emoji}
+                  alt="emoji applaudissement"
+                />
+              </p>
+            </>
+          ) : (
+            <div>Loading...</div>
+          )}
+        </div>
       </div>
     </section>
   );
